5983af6034a7d06dc72d0be6e77c8726da0962e7
Brian Merrick <Brian Merrick@DESKTOP-PIDCNU8>
2017-06-21 16:50:13 -0600

    Adding entity model to solution.

---
+++ LLC/Db/Models/Program.cs
@@ -0,0 +1,53 @@
+﻿using DbUp;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+
+namespace Db.Models
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? System.Configuration.ConfigurationManager.
+                        ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var builder = new SqlConnectionStringBuilder(connectionString);
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithVariable("DBName", builder.InitialCatalog)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+#if DEBUG
+
+                Console.ReadLine();
+
+#endif
+
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+
+            return 0;
+        }
+    }
+}
--- LLC/Db/Program.cs
+++ LLC/Db/Program.cs
@@ -1,53 +1,13 @@
-﻿using DbUp;
-using System;
-using System.Data.SqlClient;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Db
 {
     class Program
     {
-        static int Main(string[] args)
-        {
-            var connectionString =
-                args.FirstOrDefault()
-                ?? System.Configuration.ConfigurationManager.
-                        ConnectionStrings["DefaultConnection"].ConnectionString;
 
-            EnsureDatabase.For.SqlDatabase(connectionString);
-
-            var builder = new SqlConnectionStringBuilder(connectionString);
-            var upgrader =
-                DeployChanges.To
-                    .SqlDatabase(connectionString)
-                    .WithVariable("DBName", builder.InitialCatalog)
-                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                    .LogToConsole()
-                    .Build();
-
-            var result = upgrader.PerformUpgrade();
-
-            if (!result.Successful)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(result.Error);
-                Console.ResetColor();
-
-#if DEBUG
-
-                Console.ReadLine();
-
-#endif
-
-                return -1;
-            }
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Success!");
-            Console.ResetColor();
-
-            return 0;
-        }
     }
 }
