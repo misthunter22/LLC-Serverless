44cbebe5ea5985edfce8bb7add2e202546725bf3
Brian Merrick <Brian Merrick@DESKTOP-PIDCNU8>
2017-06-21 22:51:48 -0600

    Got a few tables imported into dynamoDB.

--- LLC/Db/Program.cs
+++ LLC/Db/Program.cs
@@ -1,6 +1,11 @@
-﻿using Db.Extensions;
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Runtime;
+using Db.Extensions;
 using Db.Models;
 using System;
+using System.Collections.Generic;
 
 namespace Db
 {
@@ -8,9 +13,9 @@ namespace Db
     {
         static int Main(string[] args)
         {
-            if (args.Length != 3)
+            if (args.Length < 3)
             {
-                Console.WriteLine("Usage <bucket name> <access key> <secret key>");
+                Console.WriteLine("Usage <bucket name> <access key> <secret key> [<environment>]");
                 return 1;
             }
 
@@ -20,6 +25,13 @@ namespace Db
             string bucket = args[0];
             string aKey   = args[1];
             string sKey   = args[2];
+            string env    = string.Empty;
+
+            // See if an environment is set
+            if (args.Length > 3)
+            {
+                env = args[3];
+            }
 
             // First, create all of the folders in the bucket
             // that do not use DynamoDB
@@ -32,10 +44,91 @@ namespace Db
             // Now, move the data into S3/DynamoDB
             using (var db = new LORLinkCheckerEntities())
             {
+                // Grab AWS Credentials
+                var credentials = new BasicAWSCredentials(aKey, sKey);
+                var client = new AmazonDynamoDBClient(credentials, RegionEndpoint.USWest2);
 
+                // Process Buckets
+                BucketsTable(client, db, env);
+
+                // Process Settings
+                SettingsTable(client, db, env);
             }
 
             return 0;
+        }
+
+        private static void BucketsTable(AmazonDynamoDBClient client, LORLinkCheckerEntities db, string env)
+        {
+            // Grab LLC-Buckets table
+            var table = Table.LoadTable(client, "LLC-Buckets" + env);
+
+            // Process each bucket
+            var buckets = db.S3Buckets;
+            foreach (var b in buckets)
+            {
+                // Gather up the sources
+                var sources = new List<Document>();
+                foreach (var s in b.Sources)
+                {
+                    var document = new Document(new Dictionary<string, DynamoDBEntry>()
+                    {
+                        { "Id", s.SourceId.ToString() },
+                        { "Name", s.Name },
+                        { "Description", s.Description },
+                        { "AllowLinkChecking", s.AllowLinkChecking.ToString() },
+                        { "AllowLinkExtractions", s.AllowLinkExtractions.ToString() },
+                        { "BucketId", s.S3BucketId.ToString() },
+                        { "DateCreated", s.DateCreated.ToString() }
+                    });
+
+                    sources.Add(document);
+                }
+
+                // Create the item and add it
+                var item = new Document(new Dictionary<string, DynamoDBEntry>()
+                {
+                    { "Id", b.S3BucketId.ToString() },
+                    { "Name", b.Name },
+                    { "AccessKey", b.AccessKey },
+                    { "SecretKey", b.SecretKey },
+                    { "Region", b.Region },
+                    { "SearchPrefix", b.SearchPrefix },
+                    { "DateCreated", b.DateCreated.ToString() }
+                });
+
+                item.Add("Sources", sources);
+                table.PutItem(item);
+            }
+        }
+
+        private static void SettingsTable(AmazonDynamoDBClient client, LORLinkCheckerEntities db, string env)
+        {
+            // Grab LLC-Buckets table
+            var table = Table.LoadTable(client, "LLC-Settings" + env);
+
+            foreach (var s in db.Settings)
+            {
+                // Create the item and add it
+                var item = new Document(new Dictionary<string, DynamoDBEntry>()
+                {
+                    { "Id", s.SettingId.ToString() },
+                    { "DateCreated", s.DateCreated.ToString() },
+                    { "DateModified", s.DateModified.ToString() },
+                    { "ModifiedUser", s.ModifiedUser }
+                });
+
+                if (!string.IsNullOrEmpty(s.Name))
+                    item.Add("Name", s.Name);
+
+                if (!string.IsNullOrEmpty(s.Value))
+                    item.Add("Value", s.Value);
+
+                if (!string.IsNullOrEmpty(s.Description))
+                    item.Add("Description", s.Description);
+
+                table.PutItem(item);
+            }
         }
     }
 }
