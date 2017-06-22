58e9fb3ac5599ddf8751f5efa6cefbd56ac59eed
Brian Merrick <Brian Merrick@DESKTOP-PIDCNU8>
2017-06-21 21:27:16 -0600

    Adding tables to DynamoDB.

---
+++ LLC/Db/Extensions/S3Helper.cs
@@ -0,0 +1,164 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.IO;
+
+/*
+ *  LOR Link Checker Common - S3 Helper
+ *  
+ *  Description: The S3 Helper class contains basic
+ *  configuration code for connecting to Amazon S3.
+ *  This is used all over the place, so it makes
+ *  sense to keep it all in one spot.
+ *  
+ *  Created By				: Brian Merrick
+ *  Created On				: March 12, 2017 
+ *  
+ */
+namespace Db.Extensions
+{
+    public static class S3Helper
+    {
+        public static AmazonS3Client S3Client(string accessKey, string secretKey)
+        {
+            if (string.IsNullOrEmpty(accessKey) || string.IsNullOrEmpty(secretKey))
+                return null;
+
+            // Setup the S3 connection
+            var creds = new BasicAWSCredentials(accessKey, secretKey);
+            var config = new AmazonS3Config()
+            {
+                RegionEndpoint = RegionEndpoint.USWest2
+            };
+
+            var s3Client = new AmazonS3Client(creds, config);
+            return s3Client;
+        }
+
+        public static void CreateBucket(AmazonS3Client client, string bucket, string folder)
+        {
+            try
+            {
+                client.PutBucket(bucket);
+                if (folder != null)
+                {
+                    client.PutObject(new PutObjectRequest
+                    {
+                        BucketName = bucket,
+                        Key = folder + "/",
+                        ContentBody = folder
+                    });
+                }
+            }
+            catch (Exception) { }
+        }
+
+        public static PutObjectResponse AddObject(AmazonS3Client client, string bucket, string key, Stream ms)
+        {
+            var resp = client.PutObject(new PutObjectRequest
+            {
+                BucketName = bucket,
+                Key = key,
+                InputStream = ms
+            });
+
+            return resp;
+        }
+
+        // Get object contents for a specific key
+        public static string S3ObjectContents(AmazonS3Client s3Client, string bucketName, string keyName)
+        {
+            // Track the contents to be returned
+            string content = string.Empty;
+
+            try
+            {
+                // Prepare the request to get the object's contents
+                GetObjectRequest request = new GetObjectRequest()
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+
+                // Get the object data
+                using (GetObjectResponse response = s3Client.GetObject(request))
+
+                // Read in the contents of the specific file into a string
+                using (Stream responseStream = response.ResponseStream)
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    content = reader.ReadToEnd();
+                }
+            }
+            catch (AmazonS3Exception ex)
+            {
+                if (ex.ErrorCode != null && (ex.ErrorCode.Equals("InvalidAccessKeyId") ||
+                                             ex.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Log("Please check the provided AWS Credentials.");
+                }
+
+                Log("Error in GetS3ObjectContents() method",
+                    $"Caught Exception:{ex.Message}; Response Status Code:{ex.StatusCode}; Error Code:{ex.ErrorCode}; Request ID:{ex.RequestId}",
+                    true, ex.ToString());
+            }
+            catch (Exception ex) // Catch other unexpected errors
+            {
+                content = null; // Return null for the content so we can skip processing the results since there was an error
+                Log("  Error attempting to get S3 object: " + ex.Message);
+            }
+
+            // Return the contents of the retrieved object data
+            return content;
+        }
+
+        // Downloads an object contents for a specific key on the filesystem
+        public static void DownloadS3ObjectContents(AmazonS3Client client, string bucket, string keyName, string path)
+        {
+            try
+            {
+                var request = new GetObjectRequest
+                {
+                    BucketName = bucket,
+                    Key = keyName
+                };
+
+                using (GetObjectResponse response = client.GetObject(request))
+                {
+                    response.WriteResponseStreamToFile(path);
+                }
+            }
+            catch (AmazonS3Exception ex)
+            {
+                if (ex.ErrorCode != null && (ex.ErrorCode.Equals("InvalidAccessKeyId") || ex.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Log("Please check the provided AWS Credentials.");
+                }
+
+                Log("Error in GetS3ObjectContents() method",
+                    $"Caught Exception:{ex.Message}; Response Status Code:{ex.StatusCode}; Error Code:{ex.ErrorCode}; Request ID:{ex.RequestId}",
+                    true, ex.ToString());
+            }
+            catch (Exception ex) // Catch other unexpected errors
+            {
+                Log("  Error attempting to get S3 object: " + ex.Message);
+            }
+        }
+
+        public static ListObjectsResponse ListFolder(AmazonS3Client client, string bucket, string f, string prefix)
+        {
+            var folder = string.IsNullOrEmpty(f) ? prefix : f + "/" + prefix;
+            var listFolder = client.ListObjects(bucket, folder);
+            return listFolder;
+        }
+
+        // Log output to the database log
+        private static void Log(string title, string notes = null,
+            bool isError = false, string exceptionDetails = null)
+        {
+            Console.WriteLine(title);
+        }
+    }
+}
--- LLC/Db/Program.cs
+++ LLC/Db/Program.cs
@@ -1,9 +1,6 @@
-﻿using Db.Models;
+﻿using Db.Extensions;
+using Db.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Db
 {
@@ -11,6 +8,28 @@ namespace Db
     {
         static int Main(string[] args)
         {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage <bucket name> <access key> <secret key>");
+                return 1;
+            }
+
+            // Assume the arguments are in the correct order
+            // If the access/secret keys are wrong, it won't
+            // matter anyway
+            string bucket = args[0];
+            string aKey   = args[1];
+            string sKey   = args[2];
+
+            // First, create all of the folders in the bucket
+            // that do not use DynamoDB
+            using (var client = S3Helper.S3Client(aKey, sKey))
+            {
+                S3Helper.CreateBucket(client, bucket, "Packages");
+                S3Helper.CreateBucket(client, bucket, "Screenshots");
+            }
+
+            // Now, move the data into S3/DynamoDB
             using (var db = new LORLinkCheckerEntities())
             {
 
