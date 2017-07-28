c817f326a4022b69998219b1c2845ca0d4b535aa
Brian Merrick <Brian Merrick@DESKTOP-PIDCNU8>
2017-07-27 21:27:37 -0600

    Finally got the screenshot bits to work with nodejs 6.10, woohoo!

--- LLC/serverless-screenshot/handler.js
+++ LLC/serverless-screenshot/handler.js
@@ -1,18 +1,20 @@
-const exec = require('child_process').exec;
-const crypto = require('crypto');
-const fs = require('fs');
-const AWS = require('aws-sdk');
-const validUrl = require('valid-url');
+const exec      = require('child_process').exec;
+const crypto    = require('crypto');
+const fs        = require('fs');
+const AWS       = require('aws-sdk');
+const validUrl  = require('valid-url');
+const phantomjs = require('phantomjs-prebuilt');
 
 // overall constants
-const screenWidth = 1280;
+const screenWidth  = 1280;
 const screenHeight = 1024;
 
 // screenshot the given url
 module.exports.take_screenshot = (event, context, cb) => {
-  const targetUrl = event.query.url;
-  const linkId    = event.query.linkId;
-  const statId    = event.query.statId;
+  console.info('Received event', event);
+  const targetUrl = event.queryStringParameters.url;
+  const linkId    = event.queryStringParameters.linkId;
+  const statId    = event.queryStringParameters.statId;
   const timeout   = event.stageVariables.screenshotTimeout;
 
   // check if the given url is valid
@@ -21,58 +23,60 @@ module.exports.take_screenshot = (event, context, cb) => {
     return false;
   }
 
-  const targetBucket = event.stageVariables.bucketName;
+  const targetBucket   = event.stageVariables.bucketName;
   const targetFilename = `${linkId}/${statId}.png`;
   console.log(`Snapshotting ${targetUrl} to s3://${targetBucket}/${targetFilename}`);
+  
+  var program = phantomjs.exec('screenshot.js', targetUrl, `/tmp/${statId}.png`, screenWidth, screenHeight, timeout);
+  program.stdout.pipe(process.stdout);
+  program.stderr.pipe(process.stderr);
+  program.on('exit', code => {
+		
+	if (code !== 0) {
+      cb(`phantomjs return code was ${code}`);
+      return;
+	}
+	  
+    // snapshotting succeeded, let's upload to S3
+    // read the file into buffer (perhaps make this async?)
+    const fileBuffer = fs.readFileSync(`/tmp/${statId}.png`);
 
-  // build the cmd for phantom to render the url
-  const cmd = `./phantomjs/phantomjs_linux-x86_64 --debug=yes --ignore-ssl-errors=true ./phantomjs/screenshot.js ${targetUrl} /tmp/${statId}.png ${screenWidth} ${screenHeight} ${timeout}`; // eslint-disable-line max-len
-  console.log(cmd);
-
-  // run the phantomjs command
-  exec(cmd, (error, stdout, stderr) => {
-    if (error) {
-      // the command failed (non-zero), fail the entire call
-      console.warn(`exec error: ${error}`, stdout, stderr);
-      cb(`422, please try again ${error}`);
-    } else {
-      // snapshotting succeeded, let's upload to S3
-      // read the file into buffer (perhaps make this async?)
-      const fileBuffer = fs.readFileSync(`/tmp/${statId}.png`);
-
-      // upload the file
-      const s3 = new AWS.S3();
-      s3.putObject({
-        ACL: 'public-read',
-        Key: targetFilename,
-        Body: fileBuffer,
-        Bucket: targetBucket,
-        ContentType: 'image/png',
-      }, (err) => {
-        if (err) {
-          console.warn(err);
-          cb(err);
-        } else {
-          // console.info(stderr);
-          // console.info(stdout);
-          cb(null, {
-            key: `${targetFilename}`,
-            bucket: targetBucket,
-            url: `${event.stageVariables.endpoint}${targetFilename}`,
-          });
-        }
-        return;
-      });
-    }
-  });
+    // upload the file
+    const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+    var params = {
+	  Body: fileBuffer, 
+	  Bucket: targetBucket, 
+	  Key: targetFilename
+	};
+		 
+	s3.putObject(params, function(err, data) {
+	  if (err) {
+	    console.log(err, err.stack); // an error occurred  
+	  } 
+	  else {
+		var resp = {
+		    "key": `${targetFilename}`,
+            "bucket": targetBucket,
+            "url": `${event.stageVariables.endpoint}${targetFilename}`
+		};
+		
+		cb(null, 
+		{
+		  "statusCode": 200, 
+		  "body": JSON.stringify(resp)
+		});
+	  }     
+	});
+  })
 };
 
 
 // gives a list of urls for the given snapshotted url
 module.exports.list_screenshots = (event, context, cb) => {
-  const targetUrl = event.query.linkId;
+  console.info('Received event', event);
+  const targetUrl    = event.query.linkId;
   const targetBucket = event.stageVariables.bucketName;
-  const targetPath = `${targetUrl}/`;
+  const targetPath   = `${targetUrl}/`;
 
   const s3 = new AWS.S3();
   s3.listObjects({
@@ -87,8 +91,8 @@ module.exports.list_screenshots = (event, context, cb) => {
       // for each key, get the image width and add it to the output object
       data.Contents.forEach((content) => {
         const parts = content.Key.split('/');
-        const size = parts.pop().split('.')[0];
-        urls[size] = `${event.stageVariables.endpoint}${content.Key}`;
+        const size  = parts.pop().split('.')[0];
+        urls[size]  = `${event.stageVariables.endpoint}${content.Key}`;
       });
       cb(null, urls);
     }
