259570f7edce2c8f1d7072ec120d9936f3b71a7e
Brian Merrick <Brian Merrick@DESKTOP-PIDCNU8>
2017-08-13 13:15:41 -0600

    Fixing link and scopes for application. Auth0 will now return all of the name info as well as the custom permission claims for the user/application

--- react-llc/src/Admin/Logs.js
+++ react-llc/src/Admin/Logs.js
@@ -10,7 +10,7 @@ class Logs extends Component {
           isAuthenticated() && (
 		    <div>
               <h3>Logs</h3>
-			  <span>Logs are stored under <a href="https://us-west-2.console.aws.amazon.com/cloudwatch/home?region=us-west-2#" target="_blank">CloudWatch</a> for the account the application is
+			  <span>Logs are stored under <a href="https://us-west-2.console.aws.amazon.com/cloudwatch/home?region=us-west-2#logs:" target="_blank">CloudWatch</a> for the account the application is
 			        running under. The best place to start looking is in the particular
 					<a href="https://us-west-2.console.aws.amazon.com/lambda/home?region=us-west-2#/functions" target="_blank"> lambda function </a> 
 					that is failing. Look for this page to be updated with convenience
