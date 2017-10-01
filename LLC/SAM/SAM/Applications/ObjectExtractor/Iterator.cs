e030dfa30fcc41c9bf068239ff774c070b25e181
Brian Merrick <Brian Merrick@DESKTOP-PIDCNU8>
2017-10-01 10:47:57 -0600

    Configuring base objects for jobs.

---
+++ LLC/SAM/SAM/Applications/ObjectExtractor/Iterator.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAM.Applications.ObjectExtractor
+{
+    public class Iterator : BaseHandler
+    {
+        [LambdaSerializer(typeof(JsonSerializer))]
+        public int Handler(object input, ILambdaContext context)
+        {
+            return 2;
+        }
+    }
+}
